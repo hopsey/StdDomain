41dda80e9f8822b92b2c23d53101fcea48aaf606
Tomasz Chmielewski <tomasz.chmielewski@performance-media.pl>
2017-01-26 14:52:21 +0100

    update VO factory

--- src/StdDomain/ValueObject/Factory.php
+++ src/StdDomain/ValueObject/Factory.php
@@ -51,7 +51,7 @@ class Factory
 
             $paramType = ($param->getType() === null ? null : (string)$param->getType());
 
-            if ($allowsNull && ($value === null)) {
+            if ($allowsNull && empty($value) && $value !== 0) {
                 $invokeParams[] = null;
                 continue;
             }
